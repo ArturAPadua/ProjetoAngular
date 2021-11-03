8e3ac7f47577a095eb56cf750ad479808ad393b3
ARTUR_ALMEIDA_DE_PADUA <11901918@DELTAB40.BARROCA>
2021-11-03 11:18:35 -0300

    home e link

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+// Importação do componet para o conteudo
+import { HomeComponent } from "./views/home/home.component";
+import { PaymentComponent } from "./views/payment/payment.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "payments",
+    component: PaymentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,37 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./components/template/header/header.component";
 
+// Components que criamos
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+import { HomeComponent } from "./views/home/home.component";
+
+// Importação do material
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from './components/template/footer/footer.component';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { PaymentComponent } from './views/payment/payment.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    PaymentComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
