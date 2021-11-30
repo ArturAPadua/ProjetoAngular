34bdb2c1d5574d3c58e2455c343aa3c8c0b8e4f4
ArturAPadua <79697041+ArturAPadua@users.noreply.github.com>
2021-11-30 16:20:25 -0300

    Delete frontend directory

--- frontend/src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({ selector: "app-root", templateUrl: "app.component.html" })
-export class AppComponent {
-  desenvolvedor = "Tuca";
-  ano = "2021";
-}
--- frontend/src/app/components/payment/payment.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Payment {
-  id?: number;
-  modelo: string;
-  cor: string;
-  placa: string;
-  concessionaria: string;
-}
